2ea16288d30b49a0c077e3fbcee5bdeda999d647
Ralph Yozzo <fedex1@gmail.com>
2015-04-04 22:19:18 -0400

    add process for nys assembly bill download

--- us/ny/scripts_openlegislation/process1.py
+++ us/ny/scripts_openlegislation/process1.py
@@ -1,27 +1,44 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import json
 import time
+import csv
 
 jsonstring=json.load(sys.stdin)
 
 #print jsonstring
+csvwriter = csv.writer(sys.stdout)
+csvwriter.writerow(['oid','otype','date','gmtime','data'])
 # print len(jsonstring['response']['results'])
 for dict1 in jsonstring['response']['results']:
     #print type(dict1)
-    print "====",dict1['oid']
+    gmtime=0
+    data=''
+    date=''
+    oid=dict1['oid']
     #print "====",dict1['data']['action']['date']
-    print "====",dict1['otype']
+    otype=dict1['otype']
     if dict1['otype'] == 'bill':
-        print "actions",len(dict1['data']['bill']['actions'])
+        #print "actions",len(dict1['data']['bill']['actions'])
+        data = str(dict1['data']['bill']['actions'])
+        date=time.strftime("%x %X",time.gmtime(float(dict1['data']['bill']['actions'][0]['date'])/1000))
         #pass
     elif dict1['otype'] == 'action':
-        print time.strftime("%x %X",time.gmtime(float(dict1['data']['action']['date'])/1000))
+        gmtime=float(dict1['data']['action']['date'])
+        date=time.strftime("%x %X",time.gmtime(float(dict1['data']['action']['date'])/1000))
+        data = str(dict1['data']['action'])
+    elif dict1['otype'] == 'transcript':
+        data = str(dict1['data']['transcript']['transcriptText'])
+        date=time.strftime("%x %X",time.gmtime(float(dict1['data']['transcript']['timeStamp'])/1000))
     else:
-        print '@@@@@@@@@@@@@other', dict1['otype']
+        #print '@@@@@@@@@@@@@other', dict1['otype']
+        print ('@@@@@@@@@@@@@other', dict1['otype'])
+        pass
+    csvwriter.writerow([oid,otype,date,gmtime,data])
+
     #print "====",dict1['data']
-    for item in dict1:
-        print "ITEM:", item, str(dict1[item])[0:50]
+    #for item in dict1:
+        #print "ITEM:", item, str(dict1[item])[0:50]
 
 """
 for key in jsonstring:
