2f58403019f334beb658f70c208575e72c0030bf
Ralph Yozzo <fedex1@gmail.com>
2015-04-05 18:11:17 -0400

    add queries

--- us/ny/scripts_openlegislation/getdata1.sh
+++ us/ny/scripts_openlegislation/getdata1.sh
@@ -2,4 +2,22 @@
 # curl "http://open.nysenate.gov/legislation/2.0/search.json?pageSize=1000&sort=modified&sortOrder=true&term=message+of+necessity" >messageofnecessity_0_999.csv
 # curl "http://open.nysenate.gov/legislation/2.0/search.json?pageSize=1000&pageIdx=2&sort=modified&sortOrder=true&term=message+of+necessity" >messageofnecessity_1000_1999.csv
 
-curl "http://open.nysenate.gov/legislation/2.0/search.json?pageSize=10&sort=modified&sortOrder=true&term=message+of+necessity" >messageofnecessity_10.json
+# curl "http://open.nysenate.gov/legislation/2.0/search.json?pageSize=10&sort=modified&sortOrder=true&term=message+of+necessity" >messageofnecessity_10.json
+
+# curl "http://open.nysenate.gov/legislation/2.0/search.json?pageSize=1&sort=modified&sortOrder=true&term=message+of+necessity"
+
+# transcript
+# curl "http://open.nysenate.gov/legislation/2.0/transcript/regular-session-08-03-2011.json?pageSize=1&sort=modified&sortOrder=true"
+
+# calendar
+# curl "http://open.nysenate.gov/legislation/2.0/calendar/floor-06-24-2011.json?pageSize=1&sort=modified&sortOrder=true"
+
+# bill
+# curl "http://open.nysenate.gov/legislation/2.0/bill/S1234-2011.json?pageSize=1&sort=modified&sortOrder=true"
+
+# meeting
+curl "http://open.nysenate.gov/legislation/2.0/meeting/Finance-06-24-2011.json?pageSize=1&sort=modified&sortOrder=true"
+
+# curl "http://open.nysenate.gov/legislation/2.0/bill/search.json?pageSize=10&sort=modified&sortOrder=true&term=*" 
+# curl "http://open.nysenate.gov/legislation/2.0/calendar/search.json?pageSize=10&sort=modified&sortOrder=true&term=*" 
+
