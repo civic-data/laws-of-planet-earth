96da1e794fe07b1931a463ecbed15e3a78b01a23
Ralph Yozzo <fedex1@gmail.com>
2015-04-04 20:13:44 -0400

    add get open data

--- us/ny/scripts_openlegislation/getdata1.sh
+++ us/ny/scripts_openlegislation/getdata1.sh
@@ -2,4 +2,4 @@
 # curl "http://open.nysenate.gov/legislation/2.0/search.json?pageSize=1000&sort=modified&sortOrder=true&term=message+of+necessity" >messageofnecessity_0_999.csv
 # curl "http://open.nysenate.gov/legislation/2.0/search.json?pageSize=1000&pageIdx=2&sort=modified&sortOrder=true&term=message+of+necessity" >messageofnecessity_1000_1999.csv
 
-curl "http://open.nysenate.gov/legislation/2.0/search.json?pageSize=10&sort=modified&sortOrder=true&term=message+of+necessity" >messageofnecessity_10.csv
+curl "http://open.nysenate.gov/legislation/2.0/search.json?pageSize=10&sort=modified&sortOrder=true&term=message+of+necessity" >messageofnecessity_10.json
