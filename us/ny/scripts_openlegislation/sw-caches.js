8a3a69a4a981056f1c463f6e4e7043c75201654f
Ralph Yozzo <fedex1@gmail.com>
2015-05-02 12:23:48 -0400

    add offline static file and data

--- us/ny/scripts_openlegislation/sw-caches.js
+++ us/ny/scripts_openlegislation/sw-caches.js
@@ -1,12 +1,31 @@
+CACHENAMEDYNAMIC="civic-data-dynamic"
+CACHENAMEFILES="civic-data-static-v1"
 console.log('Service Worker!!!');
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache) {
+    caches.open(CACHENAMEDYNAMIC).then(function(cache) {
       return fetch(event.request).then(function(response) {
 console.log('putting: ' + event.request);
         cache.put(event.request, response.clone());
         return response;
       });
+    })
+  );
+});
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHENAMEFILES).then(function(cache) {
+    console.log('QQQ:CACHE:');
+    console.log(cache);
+    cache.add('/getdata1_caches.html');
+    /*
+      return cache.addAll([
+        //'/',
+        // '/getdata1_caches.html'
+        // etc
+      ]);
+      */
     })
   );
 });
