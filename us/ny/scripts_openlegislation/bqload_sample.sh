dac22ae155b6b1d900ec03600d4f32d697ec48fa
Ralph Yozzo <fedex1@gmail.com>
2015-06-05 19:16:02 -0400

    add bill action

--- us/ny/scripts_openlegislation/bqload_sample.sh
+++ us/ny/scripts_openlegislation/bqload_sample.sh
@@ -1,3 +1,4 @@
 #!
-bq --project YOUR-PROJECT-HERE load --skip_leading_rows 1 nys.bills $1  date:timestamp,oid:string,otype:string,gmtime:string,data:string,senateBillNo:string,uniBill:string,law:string,sponsor:string,active:string,votes
+bq --project YOUR-PROJECT-HERE load --skip_leading_rows 1 nys.bills $1  date:timestamp,oid:string,otype:string,gmtime:string,data:string,senateBillNo:string,uniBill:string,law:string,sponsor:string,active:string,votes:string
+# bq --project YOUR-PROJECT-HERE load --skip_leading_rows 1 nys.bills_actions $1  date:timestamp,oid:string,otype:string,gmtime:string,data:string,senateBillNo:string,uniBill:string,law:string,sponsor:string,active:string,votes:string,date2:timestamp,text2:string
 
