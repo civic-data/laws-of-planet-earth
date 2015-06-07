f1b44fea8688876b5c2df9c908a2f862b0af8f00
Ralph Yozzo <fedex1@gmail.com>
2015-06-07 16:17:32 -0400

    add resolution / bill title

--- us/ny/scripts_openlegislation/api3/bqload_sample.sh
+++ us/ny/scripts_openlegislation/api3/bqload_sample.sh
@@ -1,4 +1,4 @@
 #!
 
-bq --project YOUR-PROJECT-HERE load --skip_leading_rows 1 nys.bills_v3 $1  publishedDateTime:timestamp,statusType,title,member_imgName,member_fullName,budget,rules
+bq --project YOUR-PROJECT-HERE load --skip_leading_rows 1 nys.bills_v3 $1  publishedDateTime:timestamp,statusType,title,member_imgName,member_fullName,budget,rules,signed,adopted,resolution
 
